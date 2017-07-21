9bc86930d1a95764393372ea268ad6da96cd15ea
Alex Staroselsky <AlStar01@users.noreply.github.com>
2017-07-21 08:00:39 -0600

    Added AngularFire and Firebase configuration to project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
+
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 
@@ -14,7 +18,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAR6YOjvmScPJlzItoO6zNYT5hVXDfKui8",
+    authDomain: "fireteas.firebaseapp.com",
+    databaseURL: "https://fireteas.firebaseio.com",
+    projectId: "fireteas",
+    storageBucket: "fireteas.appspot.com",
+    messagingSenderId: "992761343478"
+  }
 };
