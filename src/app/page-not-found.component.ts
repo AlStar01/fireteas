1f0c4b9e11650c4e6bab4cf8307559a8e92a06b3
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-07-23 19:16:01 -0600

    Added link back to home page ("/teas") in page not found component.

--- src/app/page-not-found.component.ts
+++ src/app/page-not-found.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
-  template: `<h2>Page not found</h2>`
+  template: `
+    <h2>Page not found</h2>
+    <p>Oops! Go back to <a routerLink="/teas">tea list</a></p>
+  `
 })
 export class PageNotFoundComponent implements OnInit {
 
