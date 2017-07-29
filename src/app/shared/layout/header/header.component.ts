1fffe4908abc78692d3ae3522291ae519713eafc
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-07-28 19:13:25 -0600

    Added stub methods for app header component.

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from '../../../core/auth/auth.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  title: string = 'Fireteas';
+
+  constructor(public authService: AuthService) {}
+
+  open(): void {}
+
+  login(): void {
+    this.authService.login();
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
