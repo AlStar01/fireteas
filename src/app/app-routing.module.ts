53f72efb3e0479b9dea4aaac4e9dde93dd410a68
Alex Staroselsky <AlStar01@users.noreply.github.com>
2017-08-03 12:35:35 -0600

    Moved base redirect of '' to '/teas' to app-routing.module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
-import { AboutComponent } from "./about/about.component";
-import { PageNotFoundComponent } from "./page-not-found.component";
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', loadChildren: 'app/teas/teas.module#TeasModule' },
   { path: 'brew', loadChildren: 'app/brew/brew.module#BrewModule' },
   { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/teas', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
 
