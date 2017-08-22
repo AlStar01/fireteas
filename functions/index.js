eb728338f6c638da895018505e2adff0580aa734
Alex Staroselsky <AlStar01@users.noreply.github.com>
2017-08-22 07:48:21 -0600

    Simplified newUser firebase function syntax.

--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,9 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.newUser = functions.auth.user().onCreate(event => {
-    const date = admin.database.ServerValue.TIMESTAMP;
+    const joinDate = admin.database.ServerValue.TIMESTAMP;
 
-    const user = Object.assign({}, event.data, { joinDate: date });
+    const user = Object.assign({}, event.data, { joinDate });
 
     admin.database().ref('/users/' + user.uid).set(user);
 });
