daab9b19eb4ceb33448580ae675af3b3f4d571e5
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-08-24 21:54:49 -0600

    Created stubs for contact module, component, and service.

---
+++ src/app/contact/contact.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactComponent } from './src/app/contact/contact.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ContactComponent]
+})
+export class ContactModule { }
