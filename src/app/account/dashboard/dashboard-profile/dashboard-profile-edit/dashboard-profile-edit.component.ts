23214b1969e196eb6a4efbabfe8abf7b04028095
Alex Staroselsky <AlStar01@users.noreply.github.com>
2017-09-02 16:42:27 -0600

    Added basic form for profile edit to allow user to edit username, bio, and various social media profile urls.

--- src/app/account/dashboard/dashboard-profile/dashboard-profile-edit/dashboard-profile-edit.component.ts
+++ src/app/account/dashboard/dashboard-profile/dashboard-profile-edit/dashboard-profile-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-profile-edit',
@@ -6,8 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-profile-edit.component.css']
 })
 export class DashboardProfileEditComponent implements OnInit {
+  profileForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.createForm();
+  }
+
+  onSubmit(): void {}
+
+  private createForm(): void {
+    this.profileForm = this.fb.group({
+      username: ['', [Validators.maxLength(64)]],
+      bio: ['', [Validators.maxLength(1000)]],
+      websiteUrl: ['', [Validators.maxLength(256)]],
+      facebookUrl: ['', [Validators.maxLength(256)]],
+      twitterUrl: ['', [Validators.maxLength(256)]]
+    });
+  }
 
   ngOnInit() {
   }
