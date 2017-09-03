1f9f35eb278995253c0f5cfd761c9882a66adc5c
Alex Staroselsky <AlStar01@users.noreply.github.com>
2017-09-02 18:13:32 -0600

    Updated newUser firebase function to only set non google provider user information.

--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,10 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.newUser = functions.auth.user().onCreate(event => {
-    const joinDate = admin.database.ServerValue.TIMESTAMP;
+  const { photoURL, displayName: username, uid } = event.data;
+  const joinDate = admin.database.ServerValue.TIMESTAMP;
 
-    const user = Object.assign({}, event.data, { joinDate });
+  const user = Object.assign({}, { joinDate, photoURL, username });
 
-    admin.database().ref('/users/' + user.uid).set(user);
+  admin.database().ref('/users/' + uid).set(user);
 });
