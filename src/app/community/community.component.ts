b39416b8f8d1ad0cacb855d72caaf6d0aee25c1a
Alex Staroselsky <AlStar01@users.noreply.github.com>
2017-09-04 18:17:24 -0600

    Added tabs to community feature.

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 
+import { Link } from '../shared/models/link';
+
 @Component({
   selector: 'app-community-brews',
   templateUrl: './community.component.html'
 })
-export class CommunityComponent { }
+export class CommunityComponent {
+  navLinks: Link[] = [
+    { path: ['brews'], label: 'Brews' }
+  ];
+}
